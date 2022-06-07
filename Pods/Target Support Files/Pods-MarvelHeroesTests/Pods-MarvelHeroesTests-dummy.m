a9aaf7c2dc30e79038e8600722c3eb2e0ffa50f6
Maria Luiza Fornagieri <marialuiza.fornagieri-ext@ab-inbev.com>
2022-06-07 02:35:19 -0300

    Using gitignore configs

--- Pods/Target Support Files/Pods-MarvelHeroesTests/Pods-MarvelHeroesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MarvelHeroesTests : NSObject
-@end
-@implementation PodsDummy_Pods_MarvelHeroesTests
-@end
--- Pods/Target Support Files/Pods-MarvelHeroesUITests/Pods-MarvelHeroesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MarvelHeroesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MarvelHeroesUITests
-@end
